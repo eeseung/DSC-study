794404177eccca40c6aafd2f903c295bb713880c
seung <seunghee9941@gmail.com>
2021-01-17 11:30:27 +0900

    node.js deploy

--- ReactStudy/study_backend/index.js
+++ ReactStudy/study_backend/index.js
@@ -2,23 +2,48 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 const path = require('path');
-const session = require('express-session');
-const dotenv = require('dotenv');
+const session= require('express-session');
 const cors = require('cors');
+const logger = require('./logger');
+const helmet = require('helmet');
+const hpp = require('hpp');
+require('dotenv').config();
 
-dotenv.config();
-const db = require('./models');
+const { sequelize } = require('./models');
 const pageRouter = require('./routes/page');
 
-db.sequelize.sync();
-
 const app = express();
-app.use(morgan('dev'));
+sequelize.sync();
+
+app.set('port', process.env.PORT || 3065)
+
+if ( process.env.NODE_ENV === 'production') {
+    app.use(morgan('combined'));
+    app.use(helmet());
+    app.use(hpp());
+} else {
+    app.use(morgan('dev'));
+}
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/', pageRouter);
 
-app.listen(3065, () => {
-    console.log('server is running on http://localhost:3065');
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    logger.info('hello');
+    logger.error(err.message);
+    next(err);
+})
+
+app.use((err, req, res) =>{
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') ==='development' ? err : {};
+    res.status(err.status || 500);
+    res.render('error');
+})
+
+app.listen(app.get('port'), () => {
+    console.log('server is running on http://localhost:', app.get('port'));
 });
